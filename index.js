4fc17c84df18b4d61f52481c06956fcff71b598f
Ali Mostafa <186329593+Ali-Mostafa-eng@users.noreply.github.com>
2026-03-11 00:20:31 +0200

    Clean up repo with .gitignore

---
+++ index.js
@@ -0,0 +1,107 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "BooksApp",
+  password: "postgres",
+  port: 5432,
+});
+db.connect();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+async function getBookById(id) {
+  try {
+    const result = await db.query("SELECT * FROM books WHERE ID=$1", [id]);
+    return result.rows[0];
+  } catch (err) {
+    console.error("Error fetching book by ID:", err);
+    return null;
+  }
+}
+app.get("/", async (req, res) => {
+  try {
+    const result = await db.query(
+      "SELECT * FROM books ORDER BY date_read DESC",
+    );
+    const books = result.rows;
+    res.render("index.ejs", { books: books });
+  } catch (err) {
+    console.error("Error fetching books:", err);
+  }
+});
+app.get("/edit/:id", async (req, res) => {
+  try {
+    const book = await getBookById(req.params.id);
+    if (!book) {
+      return res.send("Book not found");
+    }
+    res.render("edit.ejs", { book: book });
+  } catch (err) {
+    console.error("Error fetching book:", err);
+  }
+});
+app.post("/add", async (req, res) => {
+  const title = req.body["title"];
+  const author = req.body["author"];
+  const date_read = req.body["date_read"];
+
+  try {
+    const result = await axios.get(
+      `https://openlibrary.org/search.json?title=${title}`,
+    );
+    const cover_id = result.data.docs[0]?.cover_i || null;
+    await db.query(
+      "INSERT INTO books (title, author, date_read, cover_id) VALUES ($1, $2, $3, $4)",
+      [title, author, date_read, cover_id],
+    );
+    res.redirect("/");
+  } catch (err) {
+    console.error("Error adding book:", err);
+  }
+});
+app.post("/edit/:id", async (req, res) => {
+  const id = req.params.id;
+  const title = req.body["title"];
+  const author = req.body["author"];
+  const rating = req.body["rating"];
+  const notes = req.body["notes"];
+  const date_read = req.body["date_read"];
+  const currentBook = await getBookById(id);
+  const title_changed = title !== currentBook.title;
+  let cover_id = currentBook.cover_id;
+  try {
+    if (title_changed) {
+      const result = await axios.get(
+        `https://openlibrary.org/search.json?title=${title}`,
+      );
+      cover_id = result.data.docs[0]?.cover_i || null;
+    }
+    const result = await db.query(
+      "UPDATE books SET title=$1, author=$2, rating=$3, notes=$4, date_read=$5, cover_id=$6 WHERE id=$7",
+      [title, author, rating, notes, date_read, cover_id, id],
+    );
+    res.redirect("/");
+  } catch (err) {
+    console.error("Error updating book:", err);
+  }
+});
+app.get("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const result = await db.query("DELETE FROM books WHERE id=$1", [id]);
+    res.redirect("/");
+  } catch (err) {
+    console.error("Error deleting book:", err);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
